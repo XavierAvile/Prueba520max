72a5ba7be0eacca2d54d98f509d499b690faf318
ODG-ISC <ODG-ISC@ODG-ISC-PC>
2021-05-28 13:18:25 -0500

    Agregar archivos de proyecto.

---
+++ Prueba520max/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba520max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            Console.Read();adddvdveee
+        }
+    }
+}
